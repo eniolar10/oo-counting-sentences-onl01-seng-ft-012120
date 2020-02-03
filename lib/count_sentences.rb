db4bea71b056f33e32ca41566ae909ab539064e8
Ola Enol <eniolar10@gmail.com>
2020-02-03 04:27:31 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "HI Domain!".end_with?(".")
   end
 
   def question?
-
+ "HI Domain!".end_with?("?")
   end
 
   def exclamation?
-
+ "HI Domain!".end_with?("!")
   end
 
   def count_sentences
-
+"This is a string! It has three sentences. Right?".count_sentences
+binding.pry
   end
 end
